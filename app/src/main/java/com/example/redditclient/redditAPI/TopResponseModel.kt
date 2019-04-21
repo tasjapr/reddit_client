1cc99a8cc95a736e9af25af2e72d0831d3be6ed3
tasyun <tasjapr@gmail.com>
2019-04-21 11:07:26 +0300

    getting data from Reddit API implemented

---
+++ app/src/main/java/com/example/redditclient/redditAPI/TopResponseModel.kt
@@ -0,0 +1,13 @@
+package com.example.redditclient.redditAPI
+
+
+object TopResponseModel {
+
+    data class Result(val data: MainData)
+    data class MainData(val children: List<Children>)
+    data class Children(val data: Data)
+    data class Data(
+        val subreddit: String, val author: String, val created_utc: Long, val title: String,
+        val num_comments: Int, val score: Int, val url: String
+    )
+}
